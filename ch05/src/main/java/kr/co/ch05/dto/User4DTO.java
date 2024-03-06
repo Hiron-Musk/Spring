eb9975d3e035c852db8146ab08cffb593634865f
jsa7868 <chhak0503@gmail.com>
2024-03-06 16:34:25 +0900

    ch05/user5

---
+++ ch05/src/main/java/kr/co/ch05/dto/User4DTO.java
@@ -0,0 +1,70 @@
+package kr.co.ch05.dto;
+
+public class User4DTO {
+    private String uid;
+    private String name;
+    private String gender;
+    private int age;
+    private String hp;
+    private String addr;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHp() {
+        return hp;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHp(String hp) {
+        this.hp = hp;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public String toString() {
+        return "User4DTO{" +
+                "uid='" + uid + '\'' +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                ", hp='" + hp + '\'' +
+                ", addr='" + addr + '\'' +
+                '}';
+    }
+}
