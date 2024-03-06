d76dfc0e55862efd138f61d420d210a7d7a8ac6e
jsa7868 <chhak0503@gmail.com>
2024-03-06 17:40:25 +0900

    ch04/user

---
+++ ch04/src/main/java/kr/co/ch04/dao/User5DAO.java
@@ -0,0 +1,65 @@
+package kr.co.ch04.dao;
+
+
+import kr.co.ch04.dto.User5DTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class User5DAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void insertUser5(User5DTO user5DTO){
+        String sql="INSERT INTO `User5` VALUES(?,?,?,?,?)";
+
+        Object[] params={
+                user5DTO.getSeq(),
+                user5DTO.getName(),
+                user5DTO.getGender(),
+                user5DTO.getAge(),
+                user5DTO.getAddr()
+        };
+
+        jdbcTemplate.update(sql,params);
+
+    }
+
+    public User5DTO selectUser5(int seq){
+        String sql="SELECT * FROM `User5` WHERE `seq`=?";
+
+        return jdbcTemplate.queryForObject(sql,new User5RowMapper(),seq);
+    }
+
+    public List<User5DTO> selectUser5s(){
+        String sql="SELECT * FROM `User5`";
+        return jdbcTemplate.query(sql,new User5RowMapper());
+    }
+
+    public void updateUser5(User5DTO user5DTO){
+
+        String sql="UPDATE `User5` SET `name`=?, `gender`=?, `age`=?,`addr`=? WHERE `seq`=?";
+        Object[] params={
+                user5DTO.getName(),
+                user5DTO.getGender(),
+                user5DTO.getAge(),
+                user5DTO.getAddr(),
+                user5DTO.getSeq()
+        };
+
+        jdbcTemplate.update(sql,params);
+
+
+    }
+
+    public void deleteUser5(int seq){
+        String sql="DELETE FROM `User5` WHERE `seq`=?";
+
+        jdbcTemplate.update(sql,seq);
+
+    }
+
+}
